46b86e086055aa0c16d533ad9f16e1b609865d89
EuridiceUwU <euriminina@gmail.com>
2025-10-06 12:52:28 -0600

    cambio en tamaño de imagenes

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 120,
     borderRadius: 8,
-    resizeMode: "cover",
+    resizeMode: "contain",
   },
   productDetails: {
     alignItems: "flex-start",
